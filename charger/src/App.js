091b5901c698dc575784ffcdb47edd1a04e3598b
arjun girish <arjun@pixeldev.tech>
2023-11-15 13:24:34 +0100

    initial setup

---
+++ charger/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './App.css';
+import {Routes,Route} from "react-router-dom";
+import Home from "./components/Home.js";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ charger/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home(){
+  return(
+    <div>
+      <h1 className="h1">Charger Aggregator</h1>
+    </div>
+  )
+}
