c71ed614af0a26b96da53f94418b2fca8704e72c
arjun girish <arjun@pixeldev.tech>
2023-11-24 14:49:57 +0100

    routing update

--- charger/src/App.js
+++ charger/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import './App.css';
 import {Routes,Route} from "react-router-dom";
-import Home from "./components/Home.js";
+import MainScreen from "./components/MainScreen.js";
 import MainScreen from "./components/MainScreen.js";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/mainscreen" element={<MainScreen />} />
+      <Route path="/" element={<MainScreen/>} />
     </Routes>
   );
 }
